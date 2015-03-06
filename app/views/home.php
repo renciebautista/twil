eb95ee1a5c80b79a61120ae4c59eed7e6f875269
unknown <rencie.bautista@yahoo.com>
2015-03-06 13:37:38 +0800

    update

--- app/views/home.php
+++ app/views/home.php
@@ -11,11 +11,11 @@
 			<form action="/text" method="POST" role="form">
 				<div class="form-group">
 					<label for="phoneNumber">Phone Number</label>
-					<input type="text" class="form-control" id="phoneNumber" placeholder="Enter a mobile phone number">
+					<input type="text" class="form-control" name="phoneNumber" id="phoneNumber" placeholder="Enter a mobile phone number">
 				</div>
 				<div class="form-group">
 					<label for="message">Message</label>
-					<textarea class="form-control" id="message">Hello There!</textarea>
+					<textarea class="form-control" name="message" id="message" placeholder="Message"></textarea>
 				</div>
 				<button type="submit" class="btn btn-primary">Send Text</button>
 			</form>
